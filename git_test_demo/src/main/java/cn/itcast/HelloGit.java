0aecfdb36f0ef28ac8c5054eaafe3b8512120aac
wtn <wtn@163.com>
2019-03-11 20:01:14 +0800

    添加git_test_demo到本地仓库

---
+++ git_test_demo/src/main/java/cn/itcast/HelloGit.java
@@ -0,0 +1,12 @@
+package cn.itcast;
+
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("12345");
+        System.out.println("123456");
+        System.out.println("1234567");
+        System.out.println("12345678");
+        System.out.println("123456789");
+    }
+}
